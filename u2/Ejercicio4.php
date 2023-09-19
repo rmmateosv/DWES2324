15fa1d8f1673ebe0702d4feef092f721d6287ade
rmmateosv <rmmateosv@gmail.com>
2023-09-19 09:54:32 +0200

    1909_2

---
+++ u2/Ejercicio4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="Ejercicio4Detalle.php?nombre=Ana&edad=15">Ana</a>:15</li>
+        <li><a href="Ejercicio4Detalle.php?nombre=David&edad=19">David</a>:19</li>
+        <li><a href="Ejercicio4Detalle.php?nombre=Andrés&edad=23">Andrés</a>:23</li>
+    </ul>
+</body>
+</html>
---
+++ u2/Ejercicio4Detalle.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<h1>Se ha seleccionado a '.$_GET['nombre'].' que tiene '.$_GET['edad'].' años</h1>';
+    ?>
+</body>
+</html>
