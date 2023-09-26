2ced67f5f643e38fc3834d8f150b8f03706ba081
rmmateosv <rmmateosv@gmail.com>
2023-09-26 09:48:39 +0200

    2609

--- u2/Ejercicio14_tratamiento.php
+++ u2/Ejercicio14_tratamiento.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <?php
-        echo '<h1>TRATAMIENTO DE FORMULARIO EJERCICIO 14</h1';
+        echo '<h1>TRATAMIENTO DE FORMULARIO EJERCICIO 14</h1>';
+        //Chequear que todos los campos se han rellenado
+        if( empty($_POST['nombre']) or empty($_POST['apellidos']) or 
+            empty($_POST['ps']) or empty($_POST['fechaN']) or
+            empty($_POST['foto']) or empty($_POST['comentario']) 
+            or !isset($_POST['aficc'])){
+
+            echo '<h3 style="color:red;">Debes rellenar todos los campos</h3>';
+        }
     ?>
 </body>
 </html>
