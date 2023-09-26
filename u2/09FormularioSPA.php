48bdbacde79a8d2c3a90759dbd88e4f9ad74690f
rmmateosv <rmmateosv@gmail.com>
2023-09-26 11:21:05 +0200

    2609_2

---
+++ u2/09FormularioSPA.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    echo "ddd";
+    ?>
+    <form action="#" method="post">
+        <div>
+            <label>Nombre</label>
+            <input type="text" name="nombre"/>            
+        </div>
+        <div>
+            <label>Nº de acompañantes</label>
+            <input type="number" value="0"/>
+        </div>
+        <button type="submit" value="rellenar">Rellenar Datos Acompañantes</button>
+    </form>
+</body>
+</html>
