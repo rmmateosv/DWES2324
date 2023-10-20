bc13f4c8182d81e855cad59642f62e895c337801
rmmateosv <rmmateosv@gmail.com>
2023-10-20 14:02:51 +0200

    20

--- u3/Ejercicio4/Ejercicio4.php
+++ u3/Ejercicio4/Ejercicio4.php
@@ -1,3 +1,36 @@
+<?php
+require_once 'Evento.php';
+
+//VEr si está defindo el array de eventos en la cookie
+if(isset($_COOKIE['cEventos'])){
+    $eventos=unserialize($_COOKIE['cEventos']);
+}
+else{
+    //Creamos el array vacío
+    $eventos=[];
+}
+if(isset($_POST['crear'])){
+    //Chequear que lo datos están rellenos
+    if(!empty($_POST['fecha']) and !empty($_POST['hora']) and !empty($_POST['asunto'])){
+        $e = new Evento($_POST['fecha'],$_POST['hora'],$_POST['asunto']);
+        //Añadimos el evento al array
+        $eventos[]=$e;
+        //Actualizar/Crear la cookie (fecha caducidad de un mes)
+        setcookie('cEventos',serialize($eventos),time()+(30*24*60*60));
+    }
+    else{
+        $mensaje = "Error, rellena todos los campos";
+    }
+}
+if(isset($_POST['borrar'])){
+    //Borrar del array el elemento cuya posición está en $post de borrar
+    unset($eventos[$_POST['borrar']]);
+    //Reindexar array
+    array_values(($eventos));
+    //Actualizar la cookie
+    setcookie('cEventos',serialize($eventos),time()+(30*24*60*60));
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +40,11 @@
 </head>
 <body>
     <h1>CALENDARIO DE EVENTOS</h1>
+    <?php
+        if(isset($mensaje)){
+            echo '<h3 style="color:red;">'.$mensaje.'</h3>';
+        }
+    ?>
     <form action="" method="post">
         <table>
             <tr>
@@ -15,11 +53,21 @@
                 <th>Asunto</th>
                 <th>Acción</th>
             </tr>
+            <?php 
+            foreach($eventos as $clave=>$e){
+                echo '<tr>';
+                echo '<td>'.$e->getFecha().'</td>';
+                echo '<td>'.$e->getHora().'</td>';
+                echo '<td>'.$e->getAsunto().'</td>';
+                echo '<td><button type="submit" name="borrar" value="'.$clave.'">Borrar</button></td>';
+                echo '</tr>';
+            }
+            ?>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td><input type="date" name="fecha" value="<?php echo date('Y-m-d');?>"/></td>
+                <td><input type="time" name="hora" value="<?php echo date('H:i');?>"/></td>
+                <td><input type="text" name="asunto" placeholder="Asunto"/></td>
+                <td><input type="submit" name="crear" value="Añadir"/></td>
             </tr>
         </table>
     </form>
