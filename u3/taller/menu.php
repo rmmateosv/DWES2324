941f111f777635c2824abfcd247bb2ddaa3c11c2
rmmateosv <rmmateosv@gmail.com>
2023-10-30 08:47:07 +0100

    30

--- u3/taller/menu.php
+++ u3/taller/menu.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<!-- Grey with black text -->
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="pieza/controllerPieza.ph">Pieza</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
