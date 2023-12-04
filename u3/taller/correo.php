b24f6dae2d3e496afe8971002e726da78ff26859
Rosa <rmmateosv@gmail.com>
2023-12-04 08:46:54 +0000

    0412

--- u3/taller/correo.php
+++ u3/taller/correo.php
@@ -13,7 +13,7 @@ function enviarCorreo(Modelo $bd,Reparacion $r,$detalle, Propietario $propietari
         $correo->isSMTP();
         $correo->Host = 'smtp.gmail.com';
         $correo->SMTPAuth = true;
-        $correo->Username= 'rmmateosv@gmail.com';
+        //$correo->Username= 'rmmateosv@gmail.com';
         $correo->Password = '';
         $correo->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;
         $correo->Port=465;
@@ -23,10 +23,12 @@ function enviarCorreo(Modelo $bd,Reparacion $r,$detalle, Propietario $propietari
         $correo->addAddress($propietario->getEmail(),$propietario->getNombre());
         //Configuración del contenido del mensaje
         $correo->isHTML(true);
+        $correo->CharSet='UTF-8';
         $correo->Subject='Factura Reparación Nº '.$r->getId();
-        $correo->Body="<h1>hola mundo</h1>";
+        $texto = textoReparacion($r,$detalle,$propietario);
+        $correo->Body=$texto;
         $correo->AltBody="<h1>hola mundo</h1>";
-        $correo->addAttachment('../icon/delete25.png');
+        $correo->addAttachment('../icon/info.png');
         //Enviar correo
         if($correo->send()){
             $resultado=true;
@@ -37,5 +39,22 @@ function enviarCorreo(Modelo $bd,Reparacion $r,$detalle, Propietario $propietari
     }
     
     return $resultado;
+}
+function textoReparacion(Reparacion $r,$detalle,Propietario $propietario){
+    $texto = "<div style='font-weight:bold;'>Nombre:".$propietario->getNombre()."<br/>";
+    $texto .= "DNI:".$propietario->getDni()."</div>";
+    $texto .= "<div  style='font-weight:bold;'>Nº Reparación:".$r->getId()."<br/>";
+    $texto .= "Fecha".date("d/m/Y",strtotime($r->getFecha()))."</div>";
+    $texto .= "<table border='1' rules='all' width='50%'>".
+                "<tr><th>Concepto</th><th>Cantidad</th>".
+              "<th>Precio Udad</th><th>Total</th></tr>";
+    foreach($detalle as $d){
+        $texto .= "<tr><td>".$d['Concepto']."</td><td>".$d['Cantidad']."</td>".
+              "<td>".$d['Importe']."</td><td>".$d['Total']."</td></tr>";
+    }
+    $texto .= "<tr><td colspan='3'>Total Reparación</td><td>".
+                $r->getImporteTotal()."</td></tr>";
+    $texto .= "</table>";
+    return $texto;
 }
 ?>
