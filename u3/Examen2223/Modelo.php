3d9fe5f6372996dbbd27bf9efcc2f5b7f06684cf
Rosa <rmmateosv@gmail.com>
2023-12-11 08:40:52 +0000

    1112

---
+++ u3/Examen2223/Modelo.php
@@ -0,0 +1,37 @@
+<?php
+class Modelo{
+    private string $url='http://localhost/mensajes';
+    private string $us = 'root';
+    private string $ps = '';
+
+    private $conexion=null;
+
+    function __construct()
+    {
+        try{
+            $this->conexion = new PDO($this->url,$this->us,$this->ps);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+
+    /**
+     * Get the value of conexion
+     */
+    public function getConexion()
+    {
+        return $this->conexion;
+    }
+
+    /**
+     * Set the value of conexion
+     */
+    public function setConexion($conexion): self
+    {
+        $this->conexion = $conexion;
+
+        return $this;
+    }
+}
+?>
