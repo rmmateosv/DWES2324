d49f0dc6294d473b26f0590ab32fb84aeffc3fac
Rosa <rmmateosv@gmail.com>
2023-12-12 10:18:08 +0000

    1212

--- u3/Examen2223/Modelo.php
+++ u3/Examen2223/Modelo.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'Empleado.php';
+require_once 'Departamento.php';
+require_once 'Mensaje.php';
 
 class Modelo{
     private string $url='mysql:host=localhost;port=3306;dbname=mensajes';
@@ -16,17 +18,76 @@ class Modelo{
             echo $e->getMessage();
         }
     }
+    function enviarMensaje(Mensaje $m,$destinarios){
+        $resultado=false;
+        try{
+            //HAy que hacer inserts en 2 tablas => TRANSACCIÓN
+            $this->conexion->beginTransaction();
+            $consulta = $this->conexion->prepare(
+                'INSERT into mensaje values (default,?,?,?,curdate(),?)');
+            $params=array($m->getDeEmpleado(),$m->getParaDepartamento(),
+                            $m->getAsunto(),$m->getMensaje());
+            if($consulta->execute($params)){
+                //REcuperar el id del mensaje generado
+                //Hacer un insert en para para cada destinatario
+                foreach($destinarios as $d){
+                    $consulta = $this->conexion->prepare(
+                        'INSERT into para values (?,?,false)');
+                    $params = array(,$d->getIdEmp())
+                }
+            }
+        }catch(PDOException $e){
+            $this->conexion->rollBack();
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerEmpleadosDepartamento($idDep){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->prepare(
+                'select * from empleado where departamento = ?');
+            $params = array($idDep);
+            if($consulta->execute($params)){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Empleado($fila['idEmp'],
+                                    $fila['dni'],$fila['nombreEmp'], $fila['fechaNac'],
+                                    $fila['departamento'],$fila['cambiarPs']);
+                }
+            }
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerDepartamentos(){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->query('SELECT * from departamento order by nombre');
+            if($consulta->execute()){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Departamento($fila['idDep'],$fila['nombre']);                   
+                }
+            }
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
     function obtenerEmpleado(int $us){
         $resultado = null;
         try{
             $consulta = $this->conexion->prepare(
-                'SELECT * from empleado where idEmp = ?');
+                'SELECT * 
+                    from empleado e inner join departamento d
+                        on e.departamento = d.idDep 
+                    where idEmp = ?');
             $params=array($us);
-            if($consulta->execute()){
+            if($consulta->execute($params)){
                 if($fila=$consulta->fetch()){
                     $resultado = new Empleado($fila['idEmp'],
                     $fila['dni'],$fila['nombreEmp'],
-                    $fila['fechaNac'],$fila['departamento'],
+                    $fila['fechaNac'],new Departamento($fila['idDep'],$fila['nombre']),
                     $fila['cambiarPs']);
                 }
             }
--- u3/Examen2223/mensajes.php
+++ u3/Examen2223/mensajes.php
@@ -1,3 +1,42 @@
+<?php
+require_once 'Modelo.php';
+
+$bd = new Modelo();
+if($bd->getConexion()==null){
+	$mensaje = 'Error, no hay conexión con la bd';
+}
+//Chequear si hay empleado en la sesión
+session_start();
+if(isset($_SESSION['usuario'])){
+	//Hay empleado conectado
+	$empleado = $_SESSION['usuario'];
+}
+else{
+	//Redirigir a login
+	header('location:login.php');
+}
+if(isset($_POST['cerrar'])){
+	session_destroy();
+	header('location:login.php');
+}
+if(isset($_POST['Enviar'])){
+	if(empty($_POST['asunto']) or empty($_POST['mensaje'])){
+		$mensaje = 'Error, rellena asunto y mensaje';
+	}
+	else{
+		$m = new Mensaje(0,$empleado->getIdEmp(),$_POST['para'],
+		$_POST['asunto'],date('Y-m-d'),$_POST['mensaje']);
+		$destinarios = $bd->obtenerEmpleadosDepartamento($_POST['para']);
+		if($bd->enviarMensaje($m,$destinarios)){
+			$mensaje = 'Mensaje enviado';
+		}
+		else{
+			$mensaje = 'Error, mensaje no enviado';
+		}
+
+	}
+}
+?>
 <!doctype html>
 <html>
       <head>
@@ -7,15 +46,31 @@
      <body>
      		
      		<div> 
-                <h1 style='color:red;'>Mensaje si es necesario</h1> 
+                <h1 style='color:red;'>
+				<?php echo isset($mensaje)?$mensaje:'';?>
+			</h1> 
             </div>    
         	<form action="mensajes.php" method="post">              	
-            		<h1 style="color:blue;">Nuevo Mensaje</h1> 
-            		<h2 style="color:blue;">Nombre y dni del empleado</h2>             		
+            		<h1 style="color:blue;">
+					Nuevo Mensaje
+					</h1> 
+            		<h2 style="color:blue;">
+					<?php
+					echo 'Nombre:'.$empleado->getNombre().'-'.
+					'DNI:'.$empleado->getDni().'- Departamento:'.
+					$empleado->getDepartamento()->getNombre();
+					?>
+					</h2>             		
             		<hr/> 
             		<div> 
                 		<label for="para">Para</label><br/>           		
                         <select id="para" name="para">
+							<?php
+							$departamentos = $bd->obtenerDepartamentos();
+							foreach($departamentos as $d){
+								echo '<option value="'.$d->getIdDep().'">'.$d->getNombre().'</option>';	
+							}
+							?>
                         </select>  
                     </div>  
             		<div> 
